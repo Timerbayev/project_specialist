b76ba369b70464bdb59b4aec4da328eb7acc77ec
Aidar <account_service@bk.ru>
2019-12-13 11:39:47 +0600

    frontend_13122019

--- project_js/js/index.js
+++ project_js/js/index.js
@@ -1,9 +1,21 @@
 "use strict"
- let arr = ['ginegologist', 'surgeon', 'sonographer', 'pediatrician', 'internist', 'dental professional', '
- let base = document.getElementById("base");  
- let parent = document.createElement("ul");
- base.appendChild(parent);
- for(let i = 0; i <= 10; i++) {
+let arr = ['ginegologist', 'surgeon', 'sonographer']
+ let data_ = { "ginegologist":["consultation","manipulation","smear","cauterization"],
+   "surgeon": ["consultation","dissection of abscess","gypsum"],
+    "sonographer":["abdomen", "pelvis minor","echocardiography","doppler"]};
+for (let ar of arr) {	
+console.log(data_[ar])};
+ 
+let base = document.getElementById("base");
+for (let elem of arr) {
+let head = document.createElement("h1");
+head.classList.add("order");
+head.innerHTML = elem;
+base.appendChild(head);	
+let parent = document.createElement("ul");
+parent.classList.add("order");
+base.appendChild(parent);
+ for(let el of data_[elem]) {
 	 let row = document.createElement("li");
-       row.innerHTML = i;
-	   parent.appendChild(row);}
+       row.innerHTML = el;
+       parent.appendChild(row);} } 
