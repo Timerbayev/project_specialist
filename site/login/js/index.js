734aa5f3bd457668689b38095a40d7c0aedd1797
Aidar <account_service@bk.ru>
2019-12-20 20:20:33 +0600

    frontend_19122019

--- site/login/js/index.js
+++ site/login/js/index.js
@@ -1,29 +1,5 @@
 "use strict"
-let arr = ['ginegologist', 'surgeon', 'sonographer']
- let data_ = { "ginegologist":["consultation","manipulation","smear","cauterization"],
-   "surgeon": ["consultation","dissection of abscess","gypsum"],
-    "sonographer":["abdomen", "pelvis minor","echocardiography","doppler"]};
-for (let ar of arr) {	
-console.log(data_[ar])};
- 
-let base = document.getElementById("base");
-for (let elem of arr) {
-let head = document.createElement("h1");
-head.classList.add("order");
-head.innerHTML = elem;
-base.appendChild(head);	
-let parent = document.createElement("ul");
-parent.classList.add("order");
-base.appendChild(parent);
- for(let el of data_[elem]) {
-	 let row = document.createElement("li");
-       row.innerHTML = el;
-       parent.appendChild(row);} } 
-	   
-$(document).ready(function(){$("h1").click(function(){
-  $("ul").slideToggle("slow");
-  });
-  });	   
+   alert("Ok");
 	   
 	   
 	   
